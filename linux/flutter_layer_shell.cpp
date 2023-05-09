6f165d304772d4c1ecba57fa92808dc9e6f5b20a
Luca Coduri <coduri.vitor@gmail.com>
2023-05-09 15:19:08 +0200

    desktop_multi_window base

---
+++ linux/flutter_layer_shell.cpp
@@ -0,0 +1,5 @@
+//
+// Created by luca on 5/9/23.
+//
+
+#include "flutter_layer_shell.h"
---
+++ linux/flutter_layer_shell.h
@@ -0,0 +1,15 @@
+//
+// Created by luca on 5/9/23.
+//
+
+#ifndef DESKTOP_MULTI_WINDOW_FLUTTER_LAYER_SHELL_H
+#define DESKTOP_MULTI_WINDOW_FLUTTER_LAYER_SHELL_H
+
+#include "flutter_window.h"
+
+class flutter_layer_shell : public FlutterWindow {
+
+};
+
+
+#endif //DESKTOP_MULTI_WINDOW_FLUTTER_LAYER_SHELL_H
